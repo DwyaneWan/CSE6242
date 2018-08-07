4386d0d9f4c8011ad771f12440c1faae4638a8ac
Roc Wan <dwyanewan@gmail.com>
2018-08-06 22:42:27 -0700

    removed all

--- hw1/Q1/deduplicate.py
+++
@@ -1,7 +0,0 @@
-lines_seen = set() # holds lines already seen
-outfile = open("graph2.csv", "w")
-for line in open("graph.csv", "r"):
-    if line not in lines_seen: # not a duplicate
-        outfile.write(line)
-        lines_seen.add(line)
-outfile.close()
